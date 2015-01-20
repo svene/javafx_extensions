42cdad4d14c5358ab0ba24d4e8f1906981b78cc6
Sven Ehrke <sven.ehrke@canoo.com>
2015-01-20 16:52:37 +0100

    added FXMLLoader2

---
+++ src/main/java/org/svenehrke/javafxextensions/fxml/FXMLLoader2.java
@@ -0,0 +1,21 @@
+package org.svenehrke.javafxextensions.fxml;
+
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class FXMLLoader2 {
+
+	public static <V, R> ViewAndRoot<V, R> loadFXML(String fxmlPath) {
+		URL resource = FXMLLoader2.class.getResource(fxmlPath);
+		final FXMLLoader loader = new FXMLLoader(resource, null);
+		try {
+			loader.load();
+			return new ViewAndRoot<>(loader.getController(), loader.getRoot() );
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+}
---
+++ src/main/java/org/svenehrke/javafxextensions/fxml/ViewAndRoot.java
@@ -0,0 +1,19 @@
+package org.svenehrke.javafxextensions.fxml;
+
+public class ViewAndRoot<V, R> {
+	private final V view;
+	private final R root;
+
+	ViewAndRoot(V view, R root) {
+		this.view = view;
+		this.root = root;
+	}
+
+	public V getView() {
+		return view;
+	}
+
+	public R getRoot() {
+		return root;
+	}
+}
